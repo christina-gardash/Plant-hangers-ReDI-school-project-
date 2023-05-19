5e1df29966a4975af4a971de65f80166e481e983
christina-gardash <christina.gardash@gmail.com>
2023-05-19 15:33:25 +0200

    feat: add fetch photos structure

--- main.js
+++ main.js
@@ -8,3 +8,25 @@ function toggleHamMenu () {
     hamburger.classList.toggle('active')
     navMenu.classList.toggle('active')
 }
+
+fetch('https://dummyjson.com/products')
+  .then(response => response.json())
+  .then(rawData => {
+    const products = rawData.products;
+
+    const ourProduct = rawData.products[25]
+    console.log(ourProduct);
+
+// Or it can be done in another way, in separate lines: 
+    // const firstProduct = products[0]
+    // console.log(firstProduct);
+
+    const productPhotos = ourProduct.images;
+    console.log('Product photos', productPhotos);
+
+    
+
+
+
+
+})
